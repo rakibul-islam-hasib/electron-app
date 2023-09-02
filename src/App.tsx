3bf04c48bae3c5689344f538297ee17c41ac755c
Hasib Hossain <boraborhasib@gmail.com>
2023-09-02 20:06:02 +0600

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+      <Link to='/about'>About </Link>
+    </div>
+  );
+};
+
+export default App;
